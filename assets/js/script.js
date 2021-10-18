d210312b3936fbecb3d9df2402ab843e2a20fc5e
pc611652003 <88979797+pc611652003@users.noreply.github.com>
2021-10-18 06:44:32 -0600

    add function updateColorCode

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,6 +11,37 @@ var loadSchedule = function () {
 // Update Local Storage
 
 // Update Color Indicator
+var updateColorCode = function () {
+    var currentTime = moment().hour();
+    var targetID = "";
+
+    // Go Through Each Time Block, 9am to 17pm
+    for (var i = 9; i < 18; i++) {
+        targetID = "#hour-" + i;
+
+        // clear Class past, present, future
+        if ($(targetID).is(".past")) {
+            $(targetID).removeClass("past");
+        }
+        if ($(targetID).is(".present")) {
+            $(targetID).removeClass("present");
+        }
+        if ($(targetID).is(".future")) {
+            $(targetID).removeClass("future");
+        }
+
+        // update the Color code
+        if (i < currentTime) {
+            $(targetID).addClass("past");
+        } else {
+            if (i == currentTime) {
+                $(targetID).addClass("present");
+            } else {
+                $(targetID).addClass("future");
+            }
+        }
+    } 
+}
 
 // Button Handler
 
@@ -18,3 +49,4 @@ var loadSchedule = function () {
 
 // Launch
 loadSchedule();
+updateColorCode();

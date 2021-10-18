1fb38e11d6fe6c85b15ddc4cac2db4888d847fc4
pc611652003 <88979797+pc611652003@users.noreply.github.com>
2021-10-18 07:50:47 -0600

    add function Update Local Storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 // Start of Declaring Global Variables
+var tasks = ["", "", "", "", "", "", "", "", ""];
 // End of Declaring Global Variables
 
 // Start of Declaring Functions
@@ -8,7 +9,23 @@ var loadSchedule = function () {
     var currentDate = moment().format("dddd MMMM Do");
     $(dateLine).text(currentDate);
 }
+
+// Load Local Storage
+var loadLocalStorage = function () {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+    console.log(tasks);
+}
+
 // Update Local Storage
+var updateLocalStorage = function (ID) {
+    var taskInfoID = "#hour-" + ID;
+    var taskInfo = $(taskInfoID).val().trim(); 
+    var index = parseInt(ID) - 9;
+    tasks[index] = taskInfo;
+    
+    console.log("Just in case");
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
 
 // Update Color Indicator
 var updateColorCode = function () {
@@ -44,6 +61,11 @@ var updateColorCode = function () {
 }
 
 // Button Handler
+$("button").click(function() {
+    var targetID = $(this).attr("id");
+    targetID = targetID.replace("button-", "");
+    updateLocalStorage(targetID);
+})
 
 // End of Declaring Functions
 
